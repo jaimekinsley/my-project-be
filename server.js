ae3ce2e98df0fb8e5ae3309e849ce1ddf69d55cd
Jaime Sanders <jaimelyn517@gmail.com>
2020-05-08 17:02:15 -0700

    updates end points

--- server.js
+++ server.js
@@ -9,11 +9,43 @@ const app = require('./lib/app');
 
 const PORT = process.env.PORT || 7890;
 
+// get all of the clouds
+
 app.get('/clouds', async(req, res) => {
   const data = await client.query('SELECT * from clouds');
 
   res.json(data.rows);
 });
+
+// get one cloud
+app.get('/clouds/:id', async(req, res) => {
+  const id = req.params.id;
+  const data = await client.query(
+    'SELECT * from clouds where id=$1',
+    [id]
+  );
+  res.json(data.rows[0]);
+});
+
+// create a song
+app.post('/clouds/', async(req, res) => {
+  try {
+    const data = await client.query(
+      `insert into clouds (name, level, is_severe, user_id)
+      values ($1, $2, $3, $4)
+      returning*;`,
+      [req.body.name, req.body.level, req.body.is_severe, 1]
+    );
+
+    res.json(data.rows[0]);
+  } catch(e) {
+    console.error(e);
+    res.json(e);
+  }
+});
+
+
+
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
