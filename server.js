10d5dcfd24ef313e46ecbfb8df4ef9d5e1314c6c
Jaime Sanders <jaimelyn517@gmail.com>
2020-05-12 22:58:58 -0700

    gets back end working

--- server.js
+++ server.js
@@ -12,22 +12,11 @@ const PORT = process.env.PORT || 7890;
 // get all of the clouds
 
 app.get('/clouds', async(req, res) => {
-  const data = await client.query('SELECT * from clouds');
-
-  // const data = await client.query(`SELECT
-  //   clouds.id,
-  //   clouds.name,
-  //   clouds.level,
-  //   clouds.is_severe,
-  //   clouds.spotter_id,
-  //   spotter.spotter,
-
-  //   FROM clouds
-
-  //   JOIN spotter
-
-  //   ON clouds.spotter_id = spotter.id`);
-
+  const data = await client.query(`
+  SELECT clouds.id, clouds.name, clouds.level, clouds.is_severe, spotter.spotter
+  FROM clouds
+  JOIN spotter
+  on clouds.spotter_id = spotter.id`);
 
   res.json(data.rows);
 });
@@ -42,23 +31,25 @@ app.get('/spotter', async(req, res) => {
 
 // get one cloud
 app.get('/clouds/:id', async(req, res) => {
-  const id = req.params.id;
-  console.log('searching for cloud', id);
-  const data = await client.query(
-    'SELECT * from clouds where id=$1',
-    [id]
-  );
+  const data = await client.query(`
+  SELECT clouds.id, clouds.name, clouds.level, clouds.is_severe, spotter.spotter
+  FROM clouds
+  JOIN spotter
+  on clouds.spotter_id = spotter.id
+  where clouds.id = $1`,
+  [req.params.id]);
+
   res.json(data.rows[0]);
 });
 
 // create a cloud
-app.post('/clouds/', async(req, res) => {
+app.post('/clouds', async(req, res) => {
   try {
     const data = await client.query(
-      `insert into clouds (name, level, is_severe, user_id)
-      values ($1, $2, $3, $4)
+      `INSERT INTO clouds (user_id, name, level, is_severe, spotter_id)
+      values ($1, $2, $3, $4, $5)
       returning*;`,
-      [req.body.name, req.body.level, req.body.is_severe, 1]
+      [1, req.body.name, req.body.level, req.body.is_severe, req.body.spotter_id]
     );
 
     res.json(data.rows[0]);
@@ -69,24 +60,65 @@ app.post('/clouds/', async(req, res) => {
 });
 
 // edit a cloud's name
-app.put('/clouds/', async(req, res) => {
+app.put('/clouds/:id', async(req, res) => {
   try {
+    const id = req.params.id;
     const data = await client.query(
-      `insert into clouds (name, level, is_severe, user_id)
-      values ($1, $2, $3, $4)
-      returning*;`,
-      [req.body.name, req.body.level, req.body.is_severe, 1]
+      `UPDATE clouds
+      SET name = $2,
+          level = $3,
+          is_severe = $4
+          WHERE id = $1
+          RETURNING *;`,
+      [id, req.body.name, req.body.level, req.body.is_severe]
     );
 
     res.json(data.rows[0]);
-  } catch(e) {
-    console.error(e);
-    res.json(e);
+  } catch(err) {
+    if(err.code === '23505'){
+      res.status(400).json({
+        error: `"${req.body.name}" already exists`
+      });
+    }
+    else {
+      res.status(500).json({
+        error: err.message || err
+      });
+    }
   }
 });
 
+// delete a cloud
+app.delete('/clouds/:id', async(req, res) => {
+  const id = req.params.id;
 
-
+  try {
+    const result = await client.query(`
+    DELETE FROM clouds
+    WHERE id = $1
+    RETURNING *;`,
+    [id]);
+    if(result.rowCount === 0){
+      res.status(404).json({
+        error: 'Resource not found in database'
+      });
+    } else {
+      res.json(result.rows[0]);
+    }
+  }
+  catch(err) {
+    if(err.code === '23505') {
+      res.status(400).json({
+        error: 'Could not remove, cloud is in use.'
+      });
+    }
+    else {
+      res.status(500).json({
+        error: err.message || err
+      });
+    }
+  }
+});
 
 
 app.listen(PORT, () => {
